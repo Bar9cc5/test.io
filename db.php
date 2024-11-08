d4555b63a361514d64cf6efd2487f4f3d8ece6ef
Bar9cc5 <fanimom5@gmail.com>
2024-11-08 18:34:00 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "register";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if(!$conn){
+        die("Connection Fialed". mysqli_connect_error());
+    }   else {
+        echo "Вы успешно зарегистрировались!";
+    } ?>
+    
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("db.php");
+    $login - $_POST ["login"];
+    $pass = $_POST ["password"];
+    $repeatpass = $_POST ["repeatpassword"];
+    $email = $_POST ["email"];
+
+
+?>
